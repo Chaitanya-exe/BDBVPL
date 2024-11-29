39756eb15256cf115ac512b7c8ac83331b148088
rashmi-punia <rashmipunia909@gmail.com>
2024-11-29 15:44:41 +0530

    Phone landing page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,49 @@
+import Image from 'next/image';
 import React from 'react'
+import { FaPhone } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className="text-white md:flex flex-col flex gap-7  bg-deepTeal py-8 px-14 relative overflow-hidden">
+    <Image src={"/icons/footerillus.svg"} width={112} height={105} alt='illus' className='absolute -top-10 right-0' />
+      <div>
+        <h1 className="h1Text">About us</h1>
+        <p className="pText border-b border-white/30 py-2 text-white/80">
+          Annual savings of $18,000 on electricity costs. Boosted company’s
+          green credentials with a 35% reduction in carbon footprint
+        </p>
+        <p className="pt-3 text-white/80">
+          Copyright © 2024 BVB All rights reserved.
+        </p>
+      </div>
+      <div>
+        <h1 className="h1Text">Company links</h1>
+        <ul className=' *:text-white/80 space-y-2 py-2 *:cursor-pointer'>
+          <li className='pText hover:text-white'>Home</li>
+          <li className='pText hover:text-white'>about us</li>
+          <li className='pText hover:text-white'>services</li>
+          <li className='pText hover:text-white'>projects</li>
+          <li className='pText hover:text-white'>Contact us</li>
+        </ul>
+      </div>
+      <div>
+        <h1 className="h1Text">Contact informaiton</h1>
+        <p className="pText pt-2 text-white/80">Company Regd. Add -</p>
+        <p className="pText pt-2 text-white/80">
+          F-12 Ground floorviswakarma colony,new delhi 110044(India){" "}
+        </p>
+        <p className=" pt-2 text-white/80 flex items-center">
+          <MdEmail className="inline-flex size-6 mr-1" />
+          <span> contactbdbvpl@gmail.com </span>
+        </p>
+        <p className="pText pt-2 text-white/80 flex items-center">
+          <FaPhone className="inline-flex size-5 mr-2" />
+          <span>9312644140 </span>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
--- components/sections/QuerySec.jsx
+++ components/sections/QuerySec.jsx
@@ -1,23 +1,102 @@
-import React from 'react'
-import Conatiner from '../Conatiner'
-import Image from 'next/image';
+import React from "react";
+import Conatiner from "../Conatiner";
+import Image from "next/image";
+import { FaPhone, FaUser } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import Button from "../Button";
 
 const QuerySec = () => {
   return (
-    <div className='relative'>
+    <div className="relative">
       <Conatiner
         title={"Query section"}
         h1={"Got Questions? We’re Here to Help You Go Solar!"}
         textAllign={"text-center"}
       />
-      <Image src={"/icons/queryillus.svg"} width={120} height={112} alt='' className='absolute -right-3 -top-14 -z-10'/>
-      <form>
-        <div>
-            
+      <Image
+        src={"/icons/queryillus.svg"}
+        width={120}
+        height={112}
+        alt=""
+        className="absolute -right-3 -top-14 -z-10"
+      />
+      <Image
+        src={"/images/queryimg.png"}
+        width={210}
+        height={265}
+        alt=""
+        className="absolute left-0  -bottom-40 -z-10"
+      />
+      <form className=" *:my-2 *:bg-[#FAFAFA]">
+        <div className="border rounded flex items-center p-2 gap-2 border-text/20 focus-within:border-text/50">
+          <FaUser className="size-5 text-text/90" />
+          <input
+            type="text"
+            id="fullname"
+            name="fullName"
+            placeholder="Full Name"
+            className="capitalize w-full outline-none bg-transparent px-2"
+          />
+        </div>
+        <div className="border rounded flex items-center p-2 gap-2 border-text/20 focus-within:border-text/50">
+          <FaPhone className="size-5 text-text/90" />
+          <input
+            type="text"
+            id="ph"
+            name="ph"
+            placeholder="Phone Number"
+            className="w-full outline-none bg-transparent px-2"
+          />
+        </div>
+        <div className="border rounded flex items-center p-2 gap-2 border-text/20 focus-within:border-text/50">
+          <MdEmail className="size-6 text-text/90" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            className="w-full outline-none bg-transparent px-2"
+          />
+        </div>
+        <div className="border rounded flex items-center p-2 gap-2 border-text/20 focus-within:border-text/50">
+          <textarea
+            rows={5}
+            id="query"
+            name="query"
+            placeholder="Tell us more about your needs or questions.."
+            className="w-full  outline-none bg-transparent px-2"
+          />
+        </div>
+        <div className="border rounded flex items-center gap-2 border-text/20 focus-within:border-text/50">
+          <select
+            name="dropdown"
+            defaultValue=""
+            className="w-full capitalize bg-transparent p-2.5 h-full focus:outline-none"
+          >
+            <option value="" disabled hidden>
+              Type of inquiry
+            </option>
+            <option value="consultation" className="bg-white">
+              Consultation
+            </option>
+            <option value="design" className="bg-white">
+              Design
+            </option>
+            <option value="maintanence" className="bg-white">
+              Maintanence
+            </option>
+            <option value="installation" className="bg-white">
+              Installation
+            </option>
+          </select>
+        </div>
+        <div className="float-right pt-2">
+          <Button text={"Send Query"} handleClick={()=>{}} />
         </div>
       </form>
     </div>
   );
-}
+};
 
-export default QuerySec
+export default QuerySec;
