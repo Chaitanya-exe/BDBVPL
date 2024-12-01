b5b596b6d84d9a0514863f89d8055ea1600956c4
Aditya <solankiaditya2018@gmail.com>
2024-12-01 17:22:49 +0530

    Save changes to AboutUs page

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -1,90 +1,39 @@
 import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import ContactUs from "../contactus/page";
-import Conatiner from "@/components/Conatiner";
+import Button from "@/components/Button";
 
-const AboutUs = () => {
+export default function AboutUs() {
   return (
-    <div className="min-h-screen relative overflow-hidden">
-      <Image
-        src={"/images/herobg2.png"}
-        width={720}
-        height={400}
-        alt="img"
-        className="-z-30 absolute -top-7 h-[230px]"
-      />
-      <div className="w-[420px] h-14 bg-[#FAFAFA] absolute -left-2 z-20 top-44 -rotate-[4deg]" />
-      <div className="flex flex-col pt-14 justify-center items-center gap-2">
-        <h1 className="h1Text text-solarYellow">About us</h1>
-        <div className="flex divide-x-4 divide-solarYellow *:px-3 h2Text">
-          <Link href={"/"} className="text-white">
-            Home
-          </Link>
-          <button className="text-solarYellow">About</button>
-        </div>
-      </div>
-      <div className="my-24 *:py-6 relative">
+    <div className="my- py-2 relative overflow-hidden">
+      <div className="absolute -top-[60px] -left-6 right-0 -z-10">
         <Image
-          src={"/images/bgspot.png"}
-          width={533}
-          height={430}
-          alt="img"
-          className="-z-30 absolute bottom-10 right-8"
+          src={"/images/toprect.png"}
+          width={900}
+          height={150}
+          alt="rect"
+          className="w-full"
         />
-        <Conatiner
-          title={"About us"}
-          h1={
-            <span>
-              Your <span className="text-green">Trusted</span> Partner in{" "}
-              <span className="text-solarYellow">Solar Energy</span>
-            </span>
-          }
-          h2={
-            "Learn about our journey and how we bring solar solutions to life."
-          }
-          textAllign={"text-center"}
+      </div>
+      <div className="absolute -top-6 -z-20">
+        <Image
+          src={"/images/herobg.png"}
+          width={550}
+          height={300}
+          alt="rect"
+          className=""
         />
-        <div className="flex flex-col items-center bg-[/images/bgspot.png]">
-          <div className="grid grid-cols-2 gap-1">
-            <Image
-              src={"/images/apic1.png"}
-              width={175}
-              height={220}
-              alt="img"
-              className="row-span-2 w-full"
-            />
-            <Image
-              src={"/images/apic2.png"}
-              width={175}
-              height={110}
-              alt="img"
-              className="h-full"
-            />
-            <Image
-              src={"/images/apic3.png"}
-              width={175}
-              height={110}
-              alt="img"
-              className=" h-full"
-            />
-          </div>
-          <div className="mx-14 -mt-16 pText p-2 border-4 border-white bg-deepTeal text-white">
-            <p>
-              Focus on who you are, what you do, and why you do it in 2-3
-              sentences. Example:"At [Your Company Name], we’re passionate
-              about harnessing the power of the sun to create sustainable energy
-              solutions. With years of expertise in solar technology, we’re
-              dedicated to helping homes and businesses reduce their carbon
-              footprint and save on energy costs. We strive to make renewable
-              energy accessible, affordable, and impactful for everyone,
-              contributing to a brighter and greener tomorrow
-            </p>
-          </div>
+      </div>
+      <div className="text-white  my-20 mx-8 pl-2">
+        <h1 className="flex h1Text justify-center  text-solarYellow">
+          About Us
+        </h1>
+
+        <h2 className="h2Text mt-1">
+          Switch to solar panels and start saving the planet and your wallet
+        </h2>
+        <div className="flex items-center gap-3 my-4">
+          <Button text={"let's talk"} />
         </div>
       </div>
     </div>
   );
-};
-
-export default AboutUs;
+}
