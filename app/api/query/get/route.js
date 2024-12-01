a5c504bc4bc5bae05fae5a159ff417ebc741c634
Chaitany-exe <jeetkrishna3333@gmail.com>
2024-12-01 15:18:14 +0530

    api changes

--- app/api/query/get/route.js
+++ app/api/query/get/route.js
@@ -1,10 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-
 const userClient = new PrismaClient();
 
-async function handler(){
+async function handler(req){
     try {
-        
+        return Response.json({success:true},{status:200});
     } catch (err) {
         console.log(`Error occured: ${err}`);
         return Response.json({error: "some error occured"},{status: 500});
