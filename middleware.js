2a92c3811d69e6bc46f65e96888a0291ad7bb91c
Chaitany-exe <jeetkrishna3333@gmail.com>
2024-12-07 15:51:39 +0530

    middleware

--- middleware.js
+++ middleware.js
@@ -2,13 +2,14 @@ import { NextResponse } from "next/server";
 
 export function middleware(req){
     const { pathname } = req.nextUrl;
+    console.log(pathname)
     const host = req.headers.get('host').split('.')[0];
     if(host == 'admin'){
         const token = req.cookies.get('Authorization')?.value.split(' ')[1];
         if(!token){
-            return NextResponse.redirect(new URL('/login', req.url));
+            return NextResponse.rewrite(new URL('/login', req.url));
         }
-        return NextResponse.redirect(new URL('/dashboard', req.url));
+        return NextResponse.rewrite(new URL('/dashboard', req.url));
     }
 
     return NextResponse.next();
