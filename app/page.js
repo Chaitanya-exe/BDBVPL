056818df8e7fa67ad63a7690018ab3c81b2a2a54
Aditya <solankiaditya2018@gmail.com>
2024-12-12 17:50:33 +0530

    Message

--- app/page.js
+++ app/page.js
@@ -20,31 +20,44 @@ export default function Home() {
           width={900}
           height={800}
           alt="rect"
-          className="w-full block min-h-[380px] max-h-[650px] lg:w-screen"
+          className="block min-h-[410px] max-h-[700px] w-screen"
         />
+
+        {/* try */}
+       
+        <div className="absolute lg:-top-[78px] -top-[120px]  -left-6 -right-5 z-10">
+          <div className="bg-[#FAFAFA] relative h-52 -rotate-[3deg] min-w-[500px]">
+            <div className="bg-[#FAFAFA] absolute lg:bottom-0 lg:left-24 bottom-1 rotate-[42deg] h-28 lg:w-56 w-36 rounded-xl -z-10" />
+          </div>
+        </div>
       </div>
-      <div className="text-white max-w-[650px] my-20 md:my-40 mx-8 md:mx-20 lg:mx-32 border-l-4 pl-2 md:pl-5 border-solarYellow">
+
+      <div className="text-white max-w-[650px] my-24 md:my-56 mx-8 md:mx-20 lg:mx-32 border-l-4 pl-2 md:pl-5 border-solarYellow">
         <h1 className="h1Text  md:h1TextLg motion-preset-slide-left">
           <span className="text-solarYellow hover:motion-preset-pop motion-duration-2000">
             Power{" "}
           </span>
           Your Home,
-          <span className="text-solarYellow hover:motion-preset-pop motion-duration-2000"> Empower </span>
+          <span className="text-solarYellow hover:motion-preset-pop motion-duration-2000">
+            {" "}
+            Empower{" "}
+          </span>
           the planet.
         </h1>
         <h2 className="h2Text md:h2TextLg mt-1 motion-preset-blur-right motion-duration-2000">
           Switch to solar panels and start saving the planet and your wallet
         </h2>
         <div className="flex items-center gap-3 md:gap-5 my-4">
-        <Link href={"/contactus"}>
-          <Button text={"let's talk"} />
-        </Link>  
+          <Link href={"/contactus"}>
+            <Button text={"let's talk"} />
+          </Link>
           <button className=" text-solarYellow hover:animate-pulse border border-solarYellow rounded-full p-2.5">
             <FaPlay />
           </button>
         </div>
       </div>
-      <div className="md:flex my-28 py-10 lg:pt-24 items-center gap-10 ">
+
+      <div className="md:flex my-24 items-center gap-10 ">
         <div className="aboutImgContainer lg:w-[350px] lg:h-[400px]  mx-10 relative motion-preset-slide-right ">
           <Image
             src={"/images/aboutsectionimg.png"}
--- constants/dummy.js
+++ constants/dummy.js
@@ -1,30 +1,38 @@
 export const services = [
   {
     id: 1,
+    name: "Free Consultation",
     src: "/images/workers.png",
     desc: "Our experts will evaluate your property to design the most efficient solar solution.",
     hoverText: "",
   },
   {
     id: 2,
+    name: "Custom Solar Panel Design",
     src: "/images/workers.png",
     desc: "We create tailored solar energy systems that maximize energy production and match your budget.",
     hoverText: "",
   },
   {
     id: 3,
+    name: "Professional Installation",
+
     src: "/images/workers.png",
     desc: "Regular maintenance to keep your solar panels operating at peak performance.",
     hoverText: "",
   },
   {
     id: 4,
+    name: "Panel Maintenance",
+
     src: "/images/workers.png",
     desc: "Store excess solar energy with our advanced battery systems, ensuring power even during outages.",
     hoverText: "",
   },
   {
     id: 5,
+    name: "Panel Maintenance and Support",
+
     src: "/images/workers.png",
     desc: "Certified technicians ensure a seamless installation process with minimal disruption to your daily life.",
     hoverText: "",
