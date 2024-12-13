4550bc87c633101dd5e30e71f07db73121ffff2b
Chaitany-exe <jeetkrishna3333@gmail.com>
2024-12-13 22:46:30 +0530

    redirect

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -1,9 +1,12 @@
+'use client'
 import React from 'react'
 
 const dashboard = () => {
   return (
     <div className='flex flex-col items-center justify-between gap-3'>
-        <h1>Dashboard</h1>
+        <button>
+          Click this button
+        </button>
     </div>
   )
 }
